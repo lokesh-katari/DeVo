cd357cfbb3bce69f2c99b0925818fc0ccbca3cea
lokesh-katari <lokeshkatari921@outlook.com>
2024-09-04 22:14:56 +0530

    added metadata for the dscvr

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
-import VotingFeature from '@/components/voting/voting-feature';
+import VotingFeature from "@/components/voting/voting-feature";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  other: {
+    "dscvr:canvas:version": "vNext",
+    "og:image": "https://my-canvas.com/preview-image.png",
+  },
+};
 export default function Page() {
   return <VotingFeature />;
 }
